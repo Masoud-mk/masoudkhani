2b996184ec8fb81d5722959467a1c5feaca4e33c
Masoud-mk <msdkhani@users.noreply.github.com>
2019-02-16 19:09:31 +0330

    some style Changes

--- CV/forms.py
+++ CV/forms.py
@@ -8,4 +8,5 @@ class ContactUs(forms.Form):
                               , widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}))
     email = forms.CharField(max_length=100
                             , widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message'}))
+    message = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': 'Your Message', 'style': 'line-height: 1;'}))
